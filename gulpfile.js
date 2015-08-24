a46a2d657c43df004cef202a2488c0d759650f5d
Zmiy <ZmiyTricks@users.noreply.github.com>
2015-08-24 17:56:41 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -24,8 +24,8 @@ gulp.task('compress-img', function() {
 });
 gulp.task('build-html', function() {
     gulp.src(frontendSourcesDirectory + 'modules/**/*.jade')
-        .pipe( jade({pretty: true}) ).on('error', console.log) 
-        .pipe(gulp.dest(generatedResourcesDirectory + 'templates')) 
+        .pipe(jade({pretty: true})).on('error', console.log)
+        .pipe(gulp.dest(generatedResourcesDirectory + 'templates'));
     gulp.src(frontendSourcesDirectory + 'pages/**/*.jade')
         .pipe( jade({pretty: true}) ).on('error', console.log) 
         .pipe(gulp.dest(generatedResourcesDirectory + 'pages'))
@@ -41,8 +41,8 @@ gulp.task('copy-fonts', function() {
 gulp.task('dev-js', function() {
     gulp.src(frontendSourcesDirectory + 'modules/**/*.coffee')
         .pipe(coffee({bare: true}).on('error', console.log))
-        .pipe(gulp.dest(generatedResourcesDirectory + "tempJs"))
-    gulp.src(generatedResourcesDirectory + 'tempJs')
+        .pipe(gulp.dest(generatedResourcesDirectory + "tempJs"));
+    gulp.src(generatedResourcesDirectory + 'tempJs/**/*.js')
         .pipe(concat('bundle.js'))
         .pipe(gulp.dest(generatedResourcesDirectory));
     gulp.src(frontendSourcesDirectory + 'static/vendor-js/**/*.js')
@@ -51,10 +51,10 @@ gulp.task('dev-js', function() {
         .pipe(connect.reload());
 });
 gulp.task('dev-css', function() {
-    gulp.src([frontendSourcesDirectory + 'modules/**/*.scss'])
+    gulp.src(frontendSourcesDirectory + 'modules/**/*.scss')
         .pipe(sass())
         .pipe(gulp.dest(generatedResourcesDirectory + 'tempCss'));
-    gulp.src(generatedResourcesDirectory + 'tempCss')
+    gulp.src(generatedResourcesDirectory + 'tempCss/**/*.css')
         .pipe(concat('bundle.css'))
         .pipe(gulp.dest(generatedResourcesDirectory));
     gulp.src(frontendSourcesDirectory + 'static/vendor-css/**/*.css')
@@ -62,54 +62,28 @@ gulp.task('dev-css', function() {
         .pipe(gulp.dest(generatedResourcesDirectory))
         .pipe(connect.reload());
 });
-gulp.task('build-js', function() {
-    gulp.src(frontendSourcesDirectory + 'modules/**/*.coffee')
-        .pipe(coffee({bare: true}).on('error', console.log))
-        .pipe(gulp.dest(generatedResourcesDirectory + "tempJs"))
-    gulp.src(generatedResourcesDirectory + 'tempJs')
-        .pipe(concat('bundle.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest(generatedResourcesDirectory));
-    gulp.src(frontendSourcesDirectory + 'static/vendor-js/**/*.js')
-        .pipe(concat('vendor.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest(generatedResourcesDirectory))
-        .pipe(connect.reload());
-});
-gulp.task('build-css', function() {
-    gulp.src([frontendSourcesDirectory + 'modules/**/*.scss'])
-        .pipe(sass())
-        .pipe(gulp.dest(generatedResourcesDirectory + 'tempCss'));
-    gulp.src(generatedResourcesDirectory + 'tempCss')
-        .pipe(concat('bundle.css'))
-        .pipe(minifycss())
-        .pipe(gulp.dest(generatedResourcesDirectory));
-    gulp.src(frontendSourcesDirectory + 'static/vendor-css/**/*.css')
-        .pipe(concat('vendor.css'))
-        .pipe(minifycss())
-        .pipe(gulp.dest(generatedResourcesDirectory))
-        .pipe(connect.reload());
-});
-gulp.task('build', function(){
-    gulp.run('copy-fonts');
-    gulp.run('build-html');
+gulp.task('dev', function(){
     gulp.run('compress-img');
-    gulp.run('build-js');
-    gulp.run('build-css');
+    gulp.run('build-html');
+    gulp.run('copy-fonts');
+    gulp.run('dev-js');
+    gulp.run('dev-css');
+});
+gulp.task('release', function(){
+    gulp.run('dev');
+    gulp.src(generatedResourcesDirectory + 'bundle.css', generatedResourcesDirectory + 'vendor.css')
+        .pipe(minifycss());
+    gulp.src(generatedResourcesDirectory + 'bundle.js', generatedResourcesDirectory + 'vendor.js')
+        .pipe(uglify());
 });
 gulp.task('connect', function() {
     connect.server({
         root: generatedResourcesDirectory,
+        port: 9007,
         livereload: true
     });
 });
 gulp.task('watch', function () {
-    gulp.watch([frontendSourcesDirectory + 'modules/**/*.scss', frontendSourcesDirectory + 'static/vendor-css/**/*.css'], ['dev-css']);
-    gulp.watch([frontendSourcesDirectory + 'modules/**/*.coffee', frontendSourcesDirectory + 'static/vendor-js/**/*.js'], ['dev-js']);
-    gulp.watch([frontendSourcesDirectory + 'modules/**/*.jade', frontendSourcesDirectory + 'pages/**/*.jade'], ['build-html']);
-    gulp.watch([frontendSourcesDirectory + 'modules/img/**/*', frontendSourcesDirectory + 'static/img/**/*'], ['compress-img']);
-    gulp.watch([frontendSourcesDirectory + 'static/fonts/**/*.svg',
-                frontendSourcesDirectory + 'static/fonts/**/*.woff',
-                frontendSourcesDirectory + 'static/fonts/**/*.ttf'], ['copy-fonts']);
+    gulp.watch([frontendSourcesDirectory + '**/*'], ['dev']);
 });
-gulp.task('dev', ['connect', 'watch']);
+gulp.task('default', ['dev', 'connect', 'watch']);
